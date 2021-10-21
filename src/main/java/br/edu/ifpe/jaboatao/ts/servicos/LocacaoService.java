38c72f28a20c32d3dbe8e858e381c3327192e970
marrare <rivaldo.alvesb36@gmail.com>
2021-10-20 23:33:33 -0300

    first use JUnit

--- src/main/java/br/edu/ifpe/jaboatao/ts/servicos/LocacaoService.java
+++ src/main/java/br/edu/ifpe/jaboatao/ts/servicos/LocacaoService.java
@@ -5,10 +5,12 @@ import static br.edu.ifpe.jaboatao.ts.utils.ManipulandoDatas.novaDataComDiferenc
 
 import java.util.Date;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import br.edu.ifpe.jaboatao.ts.entidades.Bicicleta;
 import br.edu.ifpe.jaboatao.ts.entidades.Cliente;
 import br.edu.ifpe.jaboatao.ts.entidades.Locacao;
-import br.edu.ifpe.jaboatao.ts.utils.ManipulandoDatas;
 
 public class LocacaoService {
 	
@@ -44,7 +46,7 @@ public class LocacaoService {
 		locacao = service.alugarBicicleta(cliente, bicicleta);
 		
 		//Verificação / Validação
-		System.out.println(locacao.getValorLocacao());
+		System.out.println(locacao.getValorLocacao() == 50);
 		System.out.println(boDatasIguais(locacao.getDataLocacao(), new Date()));
 		System.out.println(boDatasIguais(locacao.getDataRetorno(), novaDataComDiferencaDeDias(3)));
 		
@@ -52,5 +54,28 @@ public class LocacaoService {
 		System.out.println(locacao.getBicicleta().getNome() == "Bike 01");
 		System.out.println(locacao.getBicicleta().getEstoque() == 2);
 
+	}
+	
+	@Test
+	public void primeiroTeste() {
+		
+		//Cenário
+		LocacaoService service = new LocacaoService();
+		Cliente cliente = new Cliente("Cliente 01");
+		Bicicleta bicicleta = new Bicicleta("Bike 01", 2, 50.0);
+		Locacao locacao = new Locacao();
+		
+		
+		//Ação
+		locacao = service.alugarBicicleta(cliente, bicicleta);
+		
+		//Verificação / Validação
+		Assertions.assertTrue(locacao.getValorLocacao() == 50);
+		Assertions.assertTrue(boDatasIguais(locacao.getDataLocacao(), new Date()));
+		Assertions.assertTrue(boDatasIguais(locacao.getDataRetorno(), novaDataComDiferencaDeDias(3)));
+		Assertions.assertTrue(locacao.getCliente().getNome() == "Cliente 01");
+		Assertions.assertTrue(locacao.getBicicleta().getNome() == "Bike 01");
+		Assertions.assertTrue(locacao.getBicicleta().getEstoque() == 2);
+		
 	}
 }
